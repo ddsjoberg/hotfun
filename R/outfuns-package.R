53620346322a2985ec5e42a67e9f2d1f8a5d4dee
Daniel Sjoberg <sjobergd@mskcc.org>
2019-04-02 17:13:52 -0400

    Update outfuns-package.R

--- R/outfuns-package.R
+++ R/outfuns-package.R
@@ -1,2 +1,12 @@
+#' @import dplyr
+#' @import purrr
+#' @import gt
+#' @importFrom tidyr nest unnest complete spread
+#' @importFrom tibble tibble as_tibble
+#' @importFrom rlang .data %||% set_names sym expr enexpr quo enquo parse_expr
+#' @importFrom glue glue
 #' @keywords internal
 "_PACKAGE"
+
+# allowing for the use of the dot when piping
+utils::globalVariables(".")
