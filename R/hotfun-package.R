c727e0c35faf471db8e9767d8bed09dd333359f6
Daniel Sjoberg <sjobergd@mskcc.org>
2019-04-23 14:40:09 -0400

    updated imports

--- R/hotfun-package.R
+++ R/hotfun-package.R
@@ -1,11 +1,13 @@
-#' @import dplyr
-#' @import purrr
-#' @importFrom tidyr nest unnest complete spread
-#' @importFrom tibble tibble as_tibble
-#' @importFrom rlang .data %||% set_names sym expr enexpr quo enquo parse_expr
-#' @importFrom glue glue
 #' @keywords internal
 "_PACKAGE"
 
 # allowing for the use of the dot when piping
 utils::globalVariables(".")
+
+# to be added later
+# @import dplyr
+# @import purrr
+# @importFrom tidyr nest unnest complete spread
+# @importFrom tibble tibble as_tibble
+# @importFrom rlang .data %||% set_names sym expr enexpr quo enquo parse_expr
+# @importFrom glue glue
