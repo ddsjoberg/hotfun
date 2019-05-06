b378797b109bf13bb981cde4e09a1e591238e546
Daniel Sjoberg <danield.sjoberg@gmail.com>
2019-05-05 23:55:59 -0400

    added tbl_ancova function (with printers)

--- R/hotfun-package.R
+++ R/hotfun-package.R
@@ -1,13 +1,13 @@
 #' @keywords internal
+#' @import gtsummary
+#' @import dplyr
+#' @import purrr
+#' @importFrom tidyr nest unnest complete spread
+#' @importFrom tibble tibble as_tibble
+#' @importFrom rlang .data %||% set_names sym expr enexpr quo enquo parse_expr
+#' @importFrom glue glue glue_collapse
 "_PACKAGE"
 
 # allowing for the use of the dot when piping
 utils::globalVariables(".")
 
-# to be added later
-# @import dplyr
-# @import purrr
-# @importFrom tidyr nest unnest complete spread
-# @importFrom tibble tibble as_tibble
-# @importFrom rlang .data %||% set_names sym expr enexpr quo enquo parse_expr
-# @importFrom glue glue
