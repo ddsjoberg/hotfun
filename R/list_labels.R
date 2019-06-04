d797ebcc13af4c989e5aa32e6b8097f1436835b7
Daniel Sjoberg <danield.sjoberg@gmail.com>
2019-06-03 22:24:11 -0400

    added list_labels function

---
+++ R/list_labels.R
@@ -0,0 +1,12 @@
+#' Get variable labels and store in named list
+#'
+#' @param data Data frame
+#' @author Daniel D. Sjoberg
+#' @export
+#' @examples
+#' list_labels(trial)
+
+list_labels <- function(data) {
+  map(data, ~attr(.x, "label")) %>%
+    compact()
+}
