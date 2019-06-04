3541710e209f7a85bf7d4e3474cc16065c1bdb6e
Daniel Sjoberg <danield.sjoberg@gmail.com>
2019-06-03 22:23:56 -0400

    updated function

--- R/set_derived_variables.R
+++ R/set_derived_variables.R
@@ -3,43 +3,22 @@
 #' Takes labels from the Derived Variables excel file and applies them to the
 #' passed data frame.  The function is meant to be used in the pipe.
 #'
-#' @param data data frame
-#' @param path file path to the Derived Variables excel file
-#' @param drop logical indicating whether to drop unlabelled variables
+#' @param data Data frame
+#' @param path Path to Derived Variables xls/xlsx file
+#' @param drop Logical indicating whether to drop unlabelled variables
+#' @inheritParams readxl::read_excel
 #' @author Daniel D. Sjoberg
 #' @export
+#' @examples
+#' \donttest{
+#' trial %>%
+#'   set_derived_variables("derived_variables_sjoberg.xlsx")
+#' }
 
-set_derived_variables <- function(data, path, drop =  TRUE) {
+set_derived_variables <- function(data, path, sheet = NULL, drop =  TRUE) {
   # import ---------------------------------------------------------------------
   # reading in excel file of Derived Variables
-  df_derived_variables <- readxl::read_excel(path)
-
-  # factors --------------------------------------------------------------------
-  # converting variables to factors
-  fct_levels <-
-    df_derived_variables %>%
-    select(.data$varname, .data$levels) %>%
-    spread(.data$varname, .data$levels) %>%
-    imap(~eval(parse(text = .x))) %>%
-    discard(~is.na(.) %>% all())
-
-  # checking that the variable contains all levels specifed in orderd factor vec
-  for (v in names(fct_levels)) {
-    v_levels_not_specified <-
-      stats::na.omit(data[[v]]) %>% setdiff(fct_levels[[v]])
-    if (length(v_levels_not_specified) > 0) {
-      stop(glue("Levels of column '{v}' not fully specified: ",
-                "{paste(v_levels_not_specified, collapse = ', ')}"))
-    }
-  }
-
-  # creating factor levels
-  fcts <-
-    fct_levels%>%
-    imap(~ factor(data[[.y]], .x))
-
-  # replacing character versions with the factors
-  data[names(fcts)] <- fcts
+  df_derived_variables <- readxl::read_excel(path = path, sheet = sheet)
 
   # variable labels ------------------------------------------------------------
   # converting imported derived variables into named list with labels
