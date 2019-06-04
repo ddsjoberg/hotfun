3d58e8c135d5da46d4f2446475fa73ecadfbe919
Daniel Sjoberg <danield.sjoberg@gmail.com>
2019-06-03 22:38:31 -0400

    added rlang functions

--- R/hotfun-package.R
+++ R/hotfun-package.R
@@ -5,7 +5,7 @@
 #' @importFrom tidyr nest unnest complete spread gather
 #' @importFrom glue glue glue_collapse
 #' @importFrom tibble tibble as_tibble
-# #' @importFrom rlang .data %||% set_names sym expr enexpr quo enquo parse_expr
+#' @importFrom rlang .data %||% set_names sym expr enexpr quo enquo parse_expr
 "_PACKAGE"
 
 # allowing for the use of the dot when piping
