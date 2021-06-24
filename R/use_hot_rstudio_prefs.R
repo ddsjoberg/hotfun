cf009e52b0474b821557f0d8a7baa4e5a37f6d14
ddsjoberg <danield.sjoberg@gmail.com>
2021-06-23 20:32:49 -0400

    Update use_hot_rstudio_prefs.R

--- R/use_hot_rstudio_prefs.R
+++ R/use_hot_rstudio_prefs.R
@@ -16,7 +16,11 @@ use_hot_rstudio_prefs <- function() {
          save_workspace = "never",
          show_hidden_files = TRUE,
          show_invisibles = TRUE,
-         show_last_dot_value = TRUE)
+         show_last_dot_value = TRUE,
+         margin_column = 80L,
+         show_margin = TRUE,
+         show_invisibles = TRUE,
+         show_line_numbers = TRUE)
 
   # apply preferences ----------------------------------------------------------
   rstudio.prefs::use_rstudio_prefs(!!!hot_prefs)
