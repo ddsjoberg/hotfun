ec07fc968c574e8d23b953d0b4547da20c865439
ddsjoberg <danield.sjoberg@gmail.com>
2021-07-12 20:04:47 -0400

    Update hot_script.R

--- inst/project_templates/hot_script.R
+++ inst/project_templates/hot_script.R
@@ -19,42 +19,6 @@ if (isTRUE(
       fs::path_file() %>%
       fs::path_ext_remove()
 
-    # get MSK GitHub PAT
-    msk_pat <- mskRutils::github_msk_token()
-
-    if (msk_pat != "") {
-      # set repo to private
-      gh::gh(
-        "PATCH /repos/:org/:repo",
-        org = "Analytic-Projects",
-        repo = repo_name,
-        private = TRUE,
-        .api_url = "https://github.mskcc.org/api/v3",
-        .token = msk_pat
-      )
-      usethis::ui_done("Setting GitHub repo visability to {usethis::ui_field('private')}")
-
-      # add topic phi hashtag
-      gh::gh(
-        "PUT /repos/:org/:repo/topics",
-        org = "Analytic-Projects",
-        repo = repo_name,
-        names = array("phi"),
-        .api_url = "https://github.mskcc.org/api/v3",
-        .accept = "application/vnd.github.mercy-preview+json",
-        .token = msk_pat
-      )
-      usethis::ui_done("Adding GitHub topic {usethis::ui_field('#phi')}")
-
-    }
-    else {
-      paste(
-        "Cannot finish configuring GitHub repo, because there is no MSK GitHub",
-        "PAT available. Ensure the repo visibility is set to {usethis::ui_field('private')}",
-        "and {usethis::ui_field('#phi')} is added as a topic."
-      ) %>%
-        usethis::ui_oops()
-
-      usethis::ui_todo("Setup an MSK GitHub PAT with {usethis::ui_code('?mskRutils::github_msk_token()')}")
-    }
+    hotverse::use_github_msk_private_repo(repo = repo_name, org = "Analytic-Projects")
+    hotverse::use_github_msk_phi_repo(repo = repo_name, org = "Analytic-Projects")
   }
